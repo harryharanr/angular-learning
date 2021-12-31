b9a8f76b2ac3e78293f411bb4795eb8886e2b5f3
Hariharan Ramanan <harryharanr@gmail.com>
2021-12-31 14:02:50 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'clips';
+export class AppComponent { 
+  constructor(public modal: ModalService) {}
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.{html,ts}'
+    ]
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      backgroundColor: ['disabled'],
+    }
+  }
+}
